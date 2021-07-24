972ba7a063bc06b50cdb49145be4479f0563471c
WaterFace <kevinvisserweckworth@gmail.com>
2021-07-24 00:21:50 -0400

    Cleaned up `Pipeline` now that `PipelineBuilder` is a thing

--- src/ve_pipeline.hpp
+++ src/ve_pipeline.hpp
@@ -7,49 +7,19 @@
 
 namespace ve {
 
-struct PipelineConfigInfo {
-  PipelineConfigInfo(const PipelineConfigInfo&) = delete;
-  PipelineConfigInfo& operator=(const PipelineConfigInfo&) = delete;
-
-  VkPipelineViewportStateCreateInfo viewportInfo;
-  VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
-  VkPipelineRasterizationStateCreateInfo rasterizationInfo;
-  VkPipelineMultisampleStateCreateInfo multisampleInfo;
-  VkPipelineColorBlendAttachmentState colorBlendAttachment;
-  VkPipelineColorBlendStateCreateInfo colorBlendInfo;
-  VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
-  std::vector<VkDynamicState> dynamicStateEnables;
-  VkPipelineDynamicStateCreateInfo dynamicStateInfo;
-  VkPipelineLayout pipelineLayout = VK_NULL_HANDLE;
-  VkRenderPass renderPass = VK_NULL_HANDLE;
-  uint32_t subpass = 0;
-};
-
 class Pipeline {
-  public:
-  Pipeline(Device& device, const std::string& vertFilepath, const std::string& fragFilepath,
-      const PipelineConfigInfo& configInfo);
+public:
+  Pipeline(Device &device, VkPipeline pipeline);
   ~Pipeline();
 
-  Pipeline(const Pipeline&) = delete;
-  Pipeline& operator=(const Pipeline&) = delete;
-
-  static void defaultPipelineConfigInfo(PipelineConfigInfo& configInfo);
+  Pipeline(const Pipeline &) = delete;
+  Pipeline &operator=(const Pipeline &) = delete;
 
   void bind(VkCommandBuffer cmd);
 
-  private:
-  static std::vector<char> readFile(const std::string& filepath);
-
-  void createGraphicsPipeline(
-      const std::string& vertFilepath, const std::string& fragFilepath, const PipelineConfigInfo& configInfo);
-
-  void createShaderModule(const std::vector<char>& code, VkShaderModule* shaderModule);
-
-  Device& m_device;
+private:
   VkPipeline m_graphicsPipeline;
-  VkShaderModule m_vertShaderModule;
-  VkShaderModule m_fragShaderModule;
+  Device &m_device;
 };
 
 } // namespace ve
