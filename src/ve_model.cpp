3d62a473907d1a6f96753a903a451789b4167dd0
WaterFace <kevinvisserweckworth@gmail.com>
2021-07-24 18:23:24 -0400

    Updated the `Model` class in accordance with the new `ModelLoader` class

--- src/ve_model.cpp
+++ src/ve_model.cpp
@@ -7,61 +7,6 @@
 
 namespace ve {
 
-Model::Model(Device &device, const Model::Data &data)
-    : m_device{device}, m_vertexBuffer{device.getAllocator()}, m_indexBuffer{device.getAllocator()} {
-  createVertexBuffers(data.vertices);
-  createIndexBuffers(data.indices);
-}
-Model::~Model() {
-  // The `ve::Buffer`s should clean themselves up when they falls out of scope
-}
-
-void Model::createVertexBuffers(const std::vector<Vertex> &vertices) {
-  m_vertexCount = static_cast<uint32_t>(vertices.size());
-
-  assert(m_vertexCount >= 3 && "Need at least 3 vertices in a model");
-
-  VkDeviceSize bufferSize = sizeof(vertices[0]) * m_vertexCount;
-
-  Buffer stagingBuffer{m_device.getAllocator()};
-  stagingBuffer.create(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, 0, VMA_MEMORY_USAGE_CPU_ONLY);
-  stagingBuffer.write((void *)vertices.data(), bufferSize);
-
-  m_vertexBuffer.create(
-      bufferSize, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT, 0, VMA_MEMORY_USAGE_GPU_ONLY);
-
-  // TODO: Don't block while copying
-  m_device.copyBuffer(stagingBuffer.buffer, m_vertexBuffer.buffer, bufferSize);
-}
-
-void Model::createIndexBuffers(const std::vector<uint32_t> &indices) {
-  m_indexCount = static_cast<uint32_t>(indices.size());
-
-  assert(m_indexCount >= 3 && "Need at least 3 indices in a model");
-
-  VkDeviceSize bufferSize = sizeof(indices[0]) * m_indexCount;
-
-  Buffer stagingBuffer{m_device.getAllocator()};
-  stagingBuffer.create(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, 0, VMA_MEMORY_USAGE_CPU_ONLY);
-  stagingBuffer.write((void *)indices.data(), bufferSize);
-
-  m_indexBuffer.create(
-      bufferSize, VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT, 0, VMA_MEMORY_USAGE_GPU_ONLY);
-
-  // TODO: Don't block while copying
-  m_device.copyBuffer(stagingBuffer.buffer, m_indexBuffer.buffer, bufferSize);
-}
-
-void Model::bind(VkCommandBuffer cmd) {
-  VkBuffer buffers[] = {m_vertexBuffer.buffer};
-  VkDeviceSize offsets[] = {0};
-  vkCmdBindVertexBuffers(cmd, 0, 1, buffers, offsets);
-
-  vkCmdBindIndexBuffer(cmd, m_indexBuffer.buffer, 0, VK_INDEX_TYPE_UINT32);
-}
-
-void Model::draw(VkCommandBuffer cmd) { vkCmdDrawIndexed(cmd, m_indexCount, 1, 0, 0, 0); }
-
 std::vector<VkVertexInputBindingDescription> Model::Vertex::getBindingDescriptions() {
   std::vector<VkVertexInputBindingDescription> bindingDescriptions(1);
   bindingDescriptions[0].binding = 0;
--- src/ve_model.hpp
+++ src/ve_model.hpp
@@ -21,29 +21,33 @@ public:
     static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
   };
 
+  typedef uint32_t IndexType;
+  static constexpr VkIndexType vulkanIndexType = VK_INDEX_TYPE_UINT32;
+
   struct Data {
     std::vector<Vertex> vertices{};
     std::vector<uint32_t> indices{};
   };
 
-  Model(const Model &) = delete;
-  Model &operator=(const Model &) = delete;
+  Model(uint32_t vertexCount, uint32_t indexCount, IndexType firstIndex, int32_t vertexOffset)
+      : m_vertexCount{vertexCount}
+      , m_indexCount{indexCount}
+      , m_firstIndex{firstIndex}
+      , m_vertexOffset{vertexOffset} {}
+  Model()
+      : m_vertexCount{0}
+      , m_indexCount{0}
+      , m_firstIndex{0}
+      , m_vertexOffset{0} {}
+  ~Model(){};
 
-  Model(Device &device, const Model::Data &data);
-  ~Model();
-
-  void bind(VkCommandBuffer cmd);
-  void draw(VkCommandBuffer cmd);
+  void draw(VkCommandBuffer cmd) { vkCmdDrawIndexed(cmd, m_indexCount, 1, m_firstIndex, m_vertexOffset, 0); };
 
 private:
-  void createVertexBuffers(const std::vector<Vertex> &vertices);
-  void createIndexBuffers(const std::vector<uint32_t> &indices);
-
-  Device &m_device;
-  Buffer m_vertexBuffer;
-  Buffer m_indexBuffer;
   uint32_t m_vertexCount;
   uint32_t m_indexCount;
+  IndexType m_firstIndex;
+  int32_t m_vertexOffset;
 };
 
 } // namespace ve
