bfae098342ae524f6302a51da5bf6a68e4c3285b
WaterFace <kevinvisserweckworth@gmail.com>
2021-07-25 18:18:49 -0400

    started implementing shader reflection currently, almost entirely copied from https://github.com/vblanco20-1/vulkan-guide/blob/engine/extra-engine/vk_shaders.cpp

--- src/ve_shader.cpp
+++ src/ve_shader.cpp
@@ -1,19 +1,24 @@
 #include "ve_shader.hpp"
 
+#include "spirv_reflect/spirv_reflect.h"
+
+#include <cassert>
 #include <fstream>
 #include <iostream>
 
 namespace ve {
 
-Shader::Shader(Device &device, const std::string &filepath) : m_device{device} {
+Shader::Shader(Device &device, const std::string &filepath)
+    : m_device{device} {
   auto code = readFile(filepath);
   std::cout << filepath << " file size: " << code.size() << std::endl;
   createShaderModule(code, &m_shaderModule);
+  m_code = std::move(code);
 }
 
 Shader::~Shader() { vkDestroyShaderModule(m_device.device(), m_shaderModule, nullptr); }
 
-std::vector<char> Shader::readFile(const std::string &filepath) {
+std::vector<uint8_t> Shader::readFile(const std::string &filepath) {
   std::ifstream file(filepath, std::ios::ate | std::ios::binary);
 
   if (!file.is_open()) {
@@ -21,16 +26,16 @@ std::vector<char> Shader::readFile(const std::string &filepath) {
   }
 
   size_t fileSize = static_cast<size_t>(file.tellg());
-  std::vector<char> buffer(fileSize);
+  std::vector<uint8_t> buffer(fileSize);
 
   file.seekg(0);
-  file.read(buffer.data(), fileSize);
+  file.read((char *)buffer.data(), fileSize);
   file.close();
 
   return buffer;
 }
 
-void Shader::createShaderModule(const std::vector<char> &code, VkShaderModule *shaderModule) {
+void Shader::createShaderModule(const std::vector<uint8_t> &code, VkShaderModule *shaderModule) {
   VkShaderModuleCreateInfo info{};
   info.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
   info.codeSize = code.size();
@@ -39,6 +44,145 @@ void Shader::createShaderModule(const std::vector<char> &code, VkShaderModule *s
   if (vkCreateShaderModule(m_device.device(), &info, nullptr, shaderModule) != VK_SUCCESS) {
     throw std::runtime_error("Failed to create shader module");
   }
+}
+
+void ShaderEffect::addStage(ShaderModule *shaderModule, VkShaderStageFlagBits stage) {
+  m_stages.push_back({shaderModule, stage});
+}
+
+struct DescriptorSetLayoutData {
+  uint32_t setNumber;
+  VkDescriptorSetLayoutCreateInfo info;
+  std::vector<VkDescriptorSetLayoutBinding> bindings;
+};
+void ShaderEffect::reflectLayout() {
+  std::vector<DescriptorSetLayoutData> setLayouts;
+  std::vector<VkPushConstantRange> pushConstantRanges;
+
+  for (auto &s : m_stages) {
+    SpvReflectShaderModule spvModule;
+    SpvReflectResult result =
+        spvReflectCreateShaderModule(s.module->code.size() * sizeof(uint8_t), s.module->code.data(), &spvModule);
+
+    uint32_t count = 0;
+    result = spvReflectEnumerateDescriptorSets(&spvModule, &count, nullptr);
+    assert(result == SPV_REFLECT_RESULT_SUCCESS);
+
+    std::vector<SpvReflectDescriptorSet *> sets(count);
+    result = spvReflectEnumerateDescriptorSets(&spvModule, &count, sets.data());
+    assert(result == SPV_REFLECT_RESULT_SUCCESS);
+
+    for (size_t i_set = 0; i_set < sets.size(); ++i_set) {
+      const SpvReflectDescriptorSet &reflectedSet = *(sets[i_set]);
+
+      DescriptorSetLayoutData layoutData{};
+
+      layoutData.bindings.resize(reflectedSet.binding_count);
+      for (uint32_t i_binding = 0; i_binding < reflectedSet.binding_count; i_binding++) {
+        const SpvReflectDescriptorBinding &reflectedBinding = *(reflectedSet.bindings[i_binding]);
+
+        VkDescriptorSetLayoutBinding &layoutBinding = layoutData.bindings[i_binding];
+        layoutBinding.binding = reflectedBinding.binding;
+        layoutBinding.descriptorType = static_cast<VkDescriptorType>(reflectedBinding.descriptor_type);
+
+        layoutBinding.descriptorCount = 1;
+        for (uint32_t i_dim = 0; i_dim < reflectedBinding.array.dims_count; ++i_dim) {
+          layoutBinding.descriptorCount *= reflectedBinding.array.dims[i_dim];
+        }
+
+        layoutBinding.stageFlags = static_cast<VkShaderStageFlagBits>(spvModule.shader_stage);
+
+        ReflectedBinding reflected;
+        reflected.binding = layoutBinding.binding;
+        reflected.set = reflectedSet.set;
+        reflected.type = layoutBinding.descriptorType;
+
+        m_bindings[reflectedBinding.name] = reflected;
+      }
+
+      layoutData.setNumber = reflectedSet.set;
+      layoutData.info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+      layoutData.info.bindingCount = reflectedSet.binding_count;
+      layoutData.info.pBindings = layoutData.bindings.data();
+
+      setLayouts.push_back(layoutData);
+    }
+
+    // Push constants
+    result = spvReflectEnumeratePushConstantBlocks(&spvModule, &count, nullptr);
+    assert(result == SPV_REFLECT_RESULT_SUCCESS);
+
+    std::vector<SpvReflectBlockVariable *> pushConstants(count);
+    result = spvReflectEnumeratePushConstantBlocks(&spvModule, &count, pushConstants.data());
+    assert(result == SPV_REFLECT_RESULT_SUCCESS);
+
+    if (count > 0) {
+      VkPushConstantRange range{};
+      range.offset = pushConstants[0]->offset;
+      range.size = pushConstants[0]->size;
+      range.stageFlags = s.stage;
+    }
+  }
+
+  std::array<DescriptorSetLayoutData, 4> mergedLayouts;
+  for (int i = 0; i < 4; i++) {
+    DescriptorSetLayoutData &layout = mergedLayouts[i];
+
+    layout.setNumber = i;
+    layout.info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+
+    std::unordered_map<int, VkDescriptorSetLayoutBinding> bindings;
+
+    for (auto &s : setLayouts) {
+      if (s.setNumber != i) {
+        continue;
+      }
+
+      for (auto &b : s.bindings) {
+        auto it = bindings.find(b.binding);
+        if (it == bindings.end()) {
+          bindings[b.binding] = b;
+        } else {
+          // merge flags
+          bindings[b.binding].stageFlags |= b.stageFlags;
+        }
+      }
+    }
+    for (auto [k, v] : bindings) {
+      layout.bindings.push_back(v);
+    }
+
+    layout.info.bindingCount = static_cast<uint32_t>(layout.bindings.size());
+    layout.info.pBindings = layout.bindings.data();
+    layout.info.flags = 0;
+    layout.info.pNext = nullptr;
+
+    if (layout.info.bindingCount > 0) {
+      vkCreateDescriptorSetLayout(m_device.device(), &layout.info, nullptr, &m_setLayouts[i]);
+    } else {
+      m_setLayouts[i] = VK_NULL_HANDLE;
+    }
+  }
+
+  VkPipelineLayoutCreateInfo layoutInfo{};
+  layoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+
+  layoutInfo.pPushConstantRanges = pushConstantRanges.data();
+  layoutInfo.pushConstantRangeCount = static_cast<uint32_t>(pushConstantRanges.size());
+
+  std::array<VkDescriptorSetLayout, 4> compactedLayouts;
+  int s = 0;
+  for (int i = 0; i < 4; i++) {
+    if (m_setLayouts[i] != VK_NULL_HANDLE) {
+      compactedLayouts[s] = m_setLayouts[i];
+      s++;
+    }
+  }
+
+  layoutInfo.setLayoutCount = s;
+  layoutInfo.pSetLayouts = compactedLayouts.data();
+
+  vkCreatePipelineLayout(m_device.device(), &layoutInfo, nullptr, &m_pipelineLayout);
 }
 
 } // namespace ve
--- src/ve_shader.hpp
+++ src/ve_shader.hpp
@@ -4,7 +4,9 @@
 
 #include <vulkan/vulkan.h>
 
+#include <array>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 namespace ve {
@@ -17,11 +19,44 @@ public:
   const VkShaderModule module() { return m_shaderModule; }
 
 private:
-  static std::vector<char> readFile(const std::string &filepath);
-  void createShaderModule(const std::vector<char> &code, VkShaderModule *shaderModule);
+  static std::vector<uint8_t> readFile(const std::string &filepath);
+  void createShaderModule(const std::vector<uint8_t> &code, VkShaderModule *shaderModule);
 
   Device &m_device;
   VkShaderModule m_shaderModule;
+  std::vector<uint8_t> m_code;
+};
+
+struct ShaderModule {
+  std::vector<uint8_t> code;
+  VkShaderModule module;
+};
+
+class ShaderEffect {
+public:
+  void addStage(ShaderModule *shaderModule, VkShaderStageFlagBits stage);
+  std::vector<VkPipelineShaderStageCreateInfo> fillStages();
+
+  void reflectLayout();
+
+private:
+  Device &m_device;
+  struct ShaderStage {
+    ShaderModule *module;
+    VkShaderStageFlagBits stage;
+  };
+  std::vector<ShaderStage> m_stages;
+  struct ReflectedBinding {
+    uint32_t set;
+    uint32_t binding;
+    VkDescriptorType type;
+  };
+  std::unordered_map<std::string, ReflectedBinding> m_bindings;
+
+  std::array<VkDescriptorSetLayout, 4> m_setLayouts;
+  std::array<uint32_t, 4> m_setHashes;
+
+  VkPipelineLayout m_pipelineLayout;
 };
 
 } // namespace ve
