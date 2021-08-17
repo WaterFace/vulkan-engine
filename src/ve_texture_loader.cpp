9731a50fe06a4149358e0b3b78e653a9f74af495
Water Face <kevinvisserweckworth@gmail.com>
2021-08-17 01:41:12 -0400

    refactored texture loading and added a cache

--- src/ve_texture_loader.cpp
+++ src/ve_texture_loader.cpp
@@ -2,18 +2,25 @@
 
 #include "tinygltf/stb_image.h"
 
+#include <cassert>
 #include <iostream>
 #include <stdexcept>
 
 namespace ve {
 
 const std::string TextureLoader::TEXTURE_PATH = "textures/";
+const uint32_t TextureLoader::MAX_TEXTURES = 1000;
 
 TextureLoader::TextureLoader(Device &device)
     : m_device{device} {
   VkSamplerCreateInfo globalSamplerCreateInfo = Texture::defaultSamplerInfo();
   vkCreateSampler(m_device.device(), &globalSamplerCreateInfo, nullptr, &m_globalSampler);
   m_globalSamplerInfo.sampler = m_globalSampler;
+
+  uint8_t blackPixel[4] = {0, 0, 0, 255};
+  m_textureCache[""] = loadFromData(blackPixel, 1, 1);
+
+  m_descriptorInfos.resize(MAX_TEXTURES, m_descriptorInfos[0]);
 }
 TextureLoader::~TextureLoader() {
   for (auto &t : m_loadedTextures) {
@@ -23,29 +30,14 @@ TextureLoader::~TextureLoader() {
   vkDestroySampler(m_device.device(), m_globalSampler, nullptr);
 }
 
-Texture TextureLoader::loadFromFile(const std::string &path) {
-  std::cout << "Loading texture " << path << std::endl;
-
-  int width, height, channels;
-
-  std::string fullPath = TEXTURE_PATH + path;
-
-  stbi_uc *pixels = stbi_load(fullPath.c_str(), &width, &height, &channels, STBI_rgb_alpha);
-
-  if (!pixels) {
-    throw std::runtime_error("Failed to load texture " + path + "!");
-    return {SIZE_MAX};
-  }
-
-  void *pixelPtr = pixels;
+Texture TextureLoader::loadFromData(void *data, uint32_t width, uint32_t height) {
+  assert(m_loadedTextures.size() < MAX_TEXTURES && "Maximum number of textures have been loaded");
   VkDeviceSize stagingBufferSize = width * height * 4;
   VkFormat format = VK_FORMAT_R8G8B8A8_SRGB;
 
   Buffer stagingBuffer{m_device.getAllocator()};
   stagingBuffer.create(stagingBufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, 0, VMA_MEMORY_USAGE_CPU_ONLY);
-  stagingBuffer.write(pixelPtr, stagingBufferSize);
-
-  stbi_image_free(pixels);
+  stagingBuffer.write(data, stagingBufferSize);
 
   VkExtent3D imageExtent{};
   imageExtent.width = static_cast<uint32_t>(width);
@@ -68,83 +60,35 @@ Texture TextureLoader::loadFromFile(const std::string &path) {
   imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
 
   auto newImage = std::make_unique<Image>(m_device.getAllocator());
-  newImage->create(&imageInfo, 0, VMA_MEMORY_USAGE_GPU_ONLY);
+  newImage->create(&imageInfo, VK_IMAGE_ASPECT_COLOR_BIT, 0, VMA_MEMORY_USAGE_GPU_ONLY);
 
-  // Layout transition:
-  VkCommandBuffer cmd = m_device.beginSingleTimeCommands();
-
-  VkImageSubresourceRange range{};
-  range.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
-  range.layerCount = imageInfo.arrayLayers;
-  range.baseArrayLayer = 0;
-  range.levelCount = imageInfo.mipLevels;
-  range.baseMipLevel = 0;
-
-  VkImageMemoryBarrier imageBarrierToTransfer{};
-  imageBarrierToTransfer.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
-  imageBarrierToTransfer.pNext = nullptr;
-
-  imageBarrierToTransfer.oldLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-  imageBarrierToTransfer.newLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
-  imageBarrierToTransfer.image = newImage->image;
-  imageBarrierToTransfer.subresourceRange = range;
-
-  imageBarrierToTransfer.srcAccessMask = 0;
-  imageBarrierToTransfer.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
-
-  vkCmdPipelineBarrier(
-      cmd,
+  m_device.imageLayoutTransition(
+      newImage->image,
+      newImage->arrayLayers(),
+      newImage->mipLevels(),
+      VK_IMAGE_ASPECT_COLOR_BIT,
+      VK_IMAGE_LAYOUT_UNDEFINED,
+      VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+      0,
+      VK_ACCESS_TRANSFER_WRITE_BIT,
       VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT,
-      VK_PIPELINE_STAGE_TRANSFER_BIT,
-      0,
-      0,
-      nullptr,
-      0,
-      nullptr,
-      1,
-      &imageBarrierToTransfer);
-
-  m_device.endSingleTimeCommands(cmd);
+      VK_PIPELINE_STAGE_TRANSFER_BIT);
 
   m_device.copyBufferToImage(stagingBuffer.buffer, newImage->image, width, height, 1);
 
-  cmd = m_device.beginSingleTimeCommands();
-
-  VkImageMemoryBarrier imageBarrierToReadable = imageBarrierToTransfer;
-  imageBarrierToReadable.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
-  imageBarrierToReadable.pNext = nullptr;
-
-  imageBarrierToReadable.oldLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
-  imageBarrierToReadable.newLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-  imageBarrierToReadable.image = newImage->image;
-  imageBarrierToReadable.subresourceRange = range;
-
-  imageBarrierToReadable.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
-  imageBarrierToReadable.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
-
-  vkCmdPipelineBarrier(
-      cmd,
+  m_device.imageLayoutTransition(
+      newImage->image,
+      newImage->arrayLayers(),
+      newImage->mipLevels(),
+      VK_IMAGE_ASPECT_COLOR_BIT,
+      VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+      VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
+      VK_ACCESS_TRANSFER_WRITE_BIT,
+      VK_ACCESS_SHADER_READ_BIT,
       VK_PIPELINE_STAGE_TRANSFER_BIT,
-      VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT,
-      0,
-      0,
-      nullptr,
-      0,
-      nullptr,
-      1,
-      &imageBarrierToReadable);
+      VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT);
 
-  m_device.endSingleTimeCommands(cmd);
-
-  VkImageViewCreateInfo imageViewInfo{};
-  imageViewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
-  imageViewInfo.pNext = nullptr;
-
-  imageViewInfo.image = newImage->image;
-  imageViewInfo.flags = 0;
-  imageViewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
-  imageViewInfo.format = format;
-  imageViewInfo.subresourceRange = range;
+  VkImageViewCreateInfo imageViewInfo = newImage->imageViewInfo();
 
   VkImageView textureImageView;
   vkCreateImageView(m_device.device(), &imageViewInfo, nullptr, &textureImageView);
@@ -181,9 +125,41 @@ Texture TextureLoader::loadFromFile(const std::string &path) {
   descriptorInfo.imageView = textureImageView;
   descriptorInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
 
-  m_descriptorInfos.push_back(descriptorInfo);
+  if (id < m_descriptorInfos.size()) {
+    m_descriptorInfos[id] = descriptorInfo;
+  } else {
+    m_descriptorInfos.push_back(descriptorInfo);
+  }
 
   return {id};
+}
+
+Texture TextureLoader::loadFromFile(const std::string &path) {
+  if (m_textureCache.find(path) != m_textureCache.end()) {
+    std::cout << "TextureLoader: Loading texture " << path << " from cache" << std::endl;
+    return m_textureCache[path];
+  }
+
+  std::cout << "TextureLoader: Loading texture " << path << " from disk" << std::endl;
+
+  int width, height, channels;
+
+  std::string fullPath = TEXTURE_PATH + path;
+
+  stbi_uc *pixels = stbi_load(fullPath.c_str(), &width, &height, &channels, STBI_rgb_alpha);
+
+  if (!pixels) {
+    throw std::runtime_error("Failed to load texture " + path + "!");
+    return {SIZE_MAX};
+  }
+
+  void *pixelPtr = pixels;
+
+  Texture texture = loadFromData(pixelPtr, width, height);
+
+  stbi_image_free(pixels);
+
+  return texture;
 }
 
 } // namespace ve
--- src/ve_texture_loader.hpp
+++ src/ve_texture_loader.hpp
@@ -5,6 +5,7 @@
 #include "ve_texture.hpp"
 
 #include <memory>
+#include <unordered_map>
 #include <vector>
 
 namespace ve {
@@ -25,11 +26,18 @@ public:
 
   static const std::string TEXTURE_PATH;
 
+  // currently, this can't be more than 4000 or so
+  static const uint32_t MAX_TEXTURES;
+
   const std::vector<VkDescriptorImageInfo> &descriptorInfos() { return m_descriptorInfos; }
   const uint32_t descriptorCount() { return static_cast<uint32_t>(m_descriptorInfos.size()); }
   const VkDescriptorImageInfo &globalSamplerInfo() { return m_globalSamplerInfo; }
 
   Texture loadFromFile(const std::string &path);
+
+  // `data` is expected to be a block of data of size width*height*4
+  // containing RGBA pixel data
+  Texture loadFromData(void *data, uint32_t width, uint32_t height);
 
 private:
   Device &m_device;
@@ -39,7 +47,7 @@ private:
 
   std::vector<VkDescriptorImageInfo> m_descriptorInfos;
 
-  // TODO: cache textures
+  std::unordered_map<std::string, Texture> m_textureCache;
   std::vector<LoadedTexture> m_loadedTextures;
 };
 
