8d4dc773432d2ed84803059ed5cdb1de3ace0fb4
Water Face <kevinvisserweckworth@gmail.com>
2021-08-20 02:35:06 -0400

    rearranged library stuff

--- src/tinygltf/tiny_gltf.cc
+++ src/tinygltf/tiny_gltf.cc
@@ -1,4 +1,8 @@
+#include "../stb_image/stb_image.h"
+#include "../stb_image/stb_image_write.h"
+
+#define TINYGLTF_NO_INCLUDE_STB_IMAGE
+#define TINYGLTF_NO_INCLUDE_STB_IMAGE_WRITE
 #define TINYGLTF_IMPLEMENTATION
-#define STB_IMAGE_IMPLEMENTATION
-#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define TINYGLTF_NO_EXTERNAL_IMAGE
 #include "tiny_gltf.h"
